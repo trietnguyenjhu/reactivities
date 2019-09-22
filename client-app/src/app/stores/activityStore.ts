744b0ee5339e29db5974b80b1b752a07faffdb9a
Triet Nguyen <trietnguyenjhu@gmail.com>
2019-09-21 22:09:44 -0400

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,27 @@ class ActivityStore {
   @observable target = "";
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+    return Object.entries( // reduce to arrays of [date, activity]
+      sortedActivities.reduce(
+        (activities, activity) => {
+          const date = activity.date.split("T")[0];
+          activities[date] = activities[date]
+            ? [...activities[date], activity]
+            : [activity];
+          return activities;
+        },
+        {} as { [key: string]: IActivity[] }
+      )
+    ); // Object.entries() converts array to arrays of [index, item]
   }
 
   @action loadActivities = async () => {
